95568532c336b2506cdee8338780786996ad050e
Cleberson Souza <cleberson@tuta.io>
2022-11-10 21:53:33 -0300

    cron

--- dk-pihole/adListUpdater.sh
+++ dk-pihole/adListUpdater.sh
@@ -10,8 +10,6 @@
 printf ".timeout 30000\\n.mode csv\\n.import \"%s\" %s\\n" "/etc/pihole/adlists.tmp" "adlist" | sqlite3 "/etc/pihole/gravity.db";
 printf ".timeout 30000\\n.mode csv\\n.import \"%s\" %s\\n" "/etc/pihole/adlistsw.tmp" "domainlist" | sqlite3 "/etc/pihole/gravity.db";
 
-
-crontab -l > adListUpdate
 echo "0 2 * * * pihole updateGravity" >> adListUpdate
 crontab adListUpdate
 rm adListUpdate
